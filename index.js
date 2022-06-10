5984d8ccb7cf174a2fd5c0b9ef8fcc8c4a415578
chotiwood <chotiwut11@gmail.com>
2022-06-10 21:44:33 +0700

    dsad

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function myFunction() {
-    this.style.background="#5832a8";
+document.getElementById("one").onclick = function () { one() };
+
+function one() {
+    document.getElementById("one").style.backgroundColor = "salmon";
+}
+
+
+
+document.getElementById("two").onclick = function () { two() };
+
+function two() {
+    document.getElementById("two").style.backgroundColor = "salmon";
+}
+
+document.getElementById("three").onclick = function () { three() };
+
+function three() {
+    document.getElementById("three").style.backgroundColor = "salmon";
+}
+
+
+document.getElementById("four").onclick = function () { four() };
+
+function four() {
+    document.getElementById("four").style.backgroundColor = "salmon";
 }
